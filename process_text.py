34ac1a1808b8f3a4f8d22bf86a982674f9ebdf85
Neural Falcon <174191741+NeuralFalconYT@users.noreply.github.com>
2026-01-23 20:32:52 +0530

    Update process_text.py

--- process_text.py
+++ process_text.py
@@ -3,6 +3,25 @@ from sentencex import segment
 import re
 import uuid
 import os
+LANGUAGE_CODE = {
+    'Akan': 'aka', 'Albanian': 'sq', 'Amharic': 'am', 'Arabic': 'ar', 'Armenian': 'hy',
+    'Assamese': 'as', 'Azerbaijani': 'az', 'Basque': 'eu', 'Bashkir': 'ba', 'Bengali': 'bn',
+    'Bosnian': 'bs', 'Bulgarian': 'bg', 'Burmese': 'my', 'Catalan': 'ca', 'Chinese': 'zh',
+    'Croatian': 'hr', 'Czech': 'cs', 'Danish': 'da', 'Dutch': 'nl', 'English': 'en',
+    'Estonian': 'et', 'Faroese': 'fo', 'Finnish': 'fi', 'French': 'fr', 'Galician': 'gl',
+    'Georgian': 'ka', 'German': 'de', 'Greek': 'el', 'Gujarati': 'gu', 'Haitian Creole': 'ht',
+    'Hausa': 'ha', 'Hebrew': 'he', 'Hindi': 'hi', 'Hungarian': 'hu', 'Icelandic': 'is',
+    'Indonesian': 'id', 'Italian': 'it', 'Japanese': 'ja', 'Kannada': 'kn', 'Kazakh': 'kk',
+    'Korean': 'ko', 'Kurdish': 'ckb', 'Kyrgyz': 'ky', 'Lao': 'lo', 'Lithuanian': 'lt',
+    'Luxembourgish': 'lb', 'Macedonian': 'mk', 'Malay': 'ms', 'Malayalam': 'ml', 'Maltese': 'mt',
+    'Maori': 'mi', 'Marathi': 'mr', 'Mongolian': 'mn', 'Nepali': 'ne', 'Norwegian': 'no',
+    'Norwegian Nynorsk': 'nn', 'Pashto': 'ps', 'Persian': 'fa', 'Polish': 'pl', 'Portuguese': 'pt',
+    'Punjabi': 'pa', 'Romanian': 'ro', 'Russian': 'ru', 'Serbian': 'sr', 'Sinhala': 'si',
+    'Slovak': 'sk', 'Slovenian': 'sl', 'Somali': 'so', 'Spanish': 'es', 'Sundanese': 'su',
+    'Swahili': 'sw', 'Swedish': 'sv', 'Tamil': 'ta', 'Telugu': 'te', 'Thai': 'th',
+    'Turkish': 'tr', 'Ukrainian': 'uk', 'Urdu': 'ur', 'Uzbek': 'uz', 'Vietnamese': 'vi',
+    'Welsh': 'cy', 'Yiddish': 'yi', 'Yoruba': 'yo', 'Zulu': 'zu'
+}
 
 # ==================================================
 # CONSTANTS
@@ -191,7 +210,9 @@ def get_tts_file_name(text, language="en"):
 # main funtion
 # ==================================================
 
-def text_chunk(text, lang_code="en", char_limit=280):
+def text_chunk(text, language="English", char_limit=280):
+    lang_code=LANGUAGE_CODE.get('English',"en")
+
     # text = clean_text(text) #because Qwen3-TTS can handle that
 
     # 🔒 Atomic quote protection
@@ -216,15 +237,15 @@ def text_chunk(text, lang_code="en", char_limit=280):
 
 # from process_text import text_chunk
 # text="Hi, this is a test"
-# chunks, tts_filename =text_chunk(text, lang_code="en", char_limit=280)
+# chunks, tts_filename =text_chunk(text,  language="English", char_limit=280)
 
 if __name__ == "__main__":
     text = "He said \"You are a looser\""  # @param {type: "string"}
 
-    lang_code = "en"  # @param {type: "string"}
+    language="English"  # @param {type: "string"}
     char_limit = 20  # @param {type: "number"}
 
-    chunks, filename = text_chunk(text, lang_code, char_limit)
+    chunks, filename = text_chunk(text, language, char_limit)
 
     print(filename)
     print(len(chunks))
